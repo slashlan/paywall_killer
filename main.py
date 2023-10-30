ba1726f564554db5eb75d43542c7e5a332d97678
Marco Pistis <65501196+slashlan@users.noreply.github.com>
2023-10-30 12:15:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def article_reader(article_url):
     # body = "  ".join(add_spacing)
 
     # Text in white for st.markdown
-    output = "''':white[" + output + "]'''"
+    output = "''':red[" + output + "]'''"
     
     return title, output
 
